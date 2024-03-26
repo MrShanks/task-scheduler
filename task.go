5ec90d66c00504995ac573a4b833fcf48044073b
MrShanks <simonestaffoli@gmail.com>
2024-03-26 13:59:25 +0100

    feat: add task property

--- task.go
+++ task.go
@@ -1,8 +1,28 @@
 package main
 
 type Task struct {
+	title string
+	desc  string
 }
 
-func NewTask() *Task {
-	return &Task{}
+func NewTask(options ...func(*Task) *Task) Task {
+	t := Task{}
+	for _, opt := range options {
+		opt(&t)
+	}
+	return t
+}
+
+func WithTitle(title string) func(*Task) *Task {
+	return func(t *Task) *Task {
+		t.title = title
+		return t
+	}
+}
+
+func WithDesc(desc string) func(*Task) *Task {
+	return func(t *Task) *Task {
+		t.desc = desc
+		return t
+	}
 }
--- task_test.go
+++ task_test.go
@@ -4,10 +4,28 @@ import "testing"
 
 func TestNewTask(t *testing.T) {
 	t.Run("New Task is empty", func(t *testing.T) {
-		want := &Task{}
+		want := Task{}
 		got := NewTask()
 
 		if got != want {
+			t.Errorf("Expected %#v, got %#v", want, got)
+		}
+	})
+
+	t.Run("New Task WithTitle has title test", func(t *testing.T) {
+		want := Task{title: "test"}
+		got := NewTask(WithTitle("test"))
+
+		if got.title != want.title {
+			t.Errorf("Expected %v, got %v", want, got)
+		}
+	})
+
+	t.Run("New Task WithDesc has desc test", func(t *testing.T) {
+		want := Task{desc: "test"}
+		got := NewTask(WithDesc("test"))
+
+		if got.desc != want.desc {
 			t.Errorf("Expected %v, got %v", want, got)
 		}
 	})
